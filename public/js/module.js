f6949b1824dd5b676185d7cf6049cf54aeb9aacc
Cade Nichols <cade@cadenichols.com>
2016-05-23 16:24:17 -0700

    end of ws

--- public/js/module.js
+++ public/js/module.js
@@ -3,11 +3,42 @@
 var app = angular.module('stripeApp', ['stripe.checkout']);
 
 
-app.controller('mainCtrl', function($scope) {
+app.controller('mainCtrl', function($scope, Payment, Bird) {
 
   $scope.doCheckout = function(token) {
-    console.log('token:', token);
+    Payment.completeCheckout(token, $scope.selectedBirdId)
+      .then(res => {
+        console.log('res:', res);
+      });
+  }
+
+  $scope.selectBird = function(id) {
+    $scope.selectedBirdId = id;
   };
 
-})
+  Bird.getAll()
+    .then(res => {
+      $scope.birds = res.data;
+    })
+    .catch(res => {
+      console.log('err:', res);
+    });
+});
+
+
+app.service('Payment', function($http) {
+  this.completeCheckout = function(token, birdId) {
+    return $http.post('/api/payment', { token: token, birdId: birdId });
+  };
+});
+
+
+app.service('Bird', function($http) {
+
+  this.getAll = () => {
+    return $http.get('/api/birds');
+  };
+
+});
+
 
--- routes/payment.js
+++ routes/payment.js
@@ -1,28 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-var stripe = require('stripe')(process.env.STRIPE_API_SECRET);
-
+var Bird = require('../models/bird');
 
 //   POST /api/payment
 router.post('/', (req, res) => {
 
-  var token = req.body.stripeToken;
-  var email = req.body.stripeEmail;
+  var token = req.body.token;
+  var birdId = req.body.birdId;
 
-  stripe.charges.create({
-    amount: 999,
-    currency: "usd",
-    source: token, // obtained with Stripe.js
-    description: `Charge for ${email}`
-  }, (err, charge) => {
-    if(err) return res.status(400).send(err);
+  console.log('birdId:', birdId);
 
-    console.log('charge:', charge);
+  Bird.findById(birdId, (err, bird) => {
+    if(err || !bird) {
+      return res.status(400).send(err || { error: 'Bird not found.' })
+    }
 
-    res.json(charge);
-    
-  });
+    bird.purchase(token, (err, charge) => {
+      if(err) return res.status(400).send(err);
+      console.log('charge:', charge);
+
+      res.send(charge);
+    });
+  });  
 });
 
 module.exports = router;
